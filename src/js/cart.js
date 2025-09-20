35f48586d213fef28f73881f08aa673d779f35fb
bright-droid <ndu24006@byui.edu>
2025-09-20 06:05:27 +0100

    cart total feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,14 +7,30 @@ const cartList = new CartList("so-cart", listElement);
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  //fix cart error and add a message if cart is empty
+  //check if cart is empty
   if (!cartItems || cartItems.length === 0) {
     document.querySelector(".cart-list").innerHTML =
       "<p>Your cart is empty.</p>";
     return;
   }
 
+  // if cart not empty, render the cart items and calculate total
   cartList.init();
+
+  const cartTotal = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.FinalPrice;
+    });
+    return total.toFixed(2);
+  };
+
+  const totalAmount = cartTotal();
+
+  document.getElementById("total").innerHTML = `${totalAmount}`;
 }
 
 renderCartContents();
+
+// When the cart page loads, and after it has pulled any cart items from local storage check to see if there is anything in the cart
+// If there are items in the cart, show the html element added above, then calculate the total of the items, create some HTML to display it ($${total}) and insert it into the element.
